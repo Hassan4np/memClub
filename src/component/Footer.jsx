582c67a69a32f60a2f3faf9ab4a72dfd1b20d6f2
Hassan Ali <hassan4np@gmail.com>
2024-06-11 18:00:43 +0600

    header complate

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,7 +1,65 @@
-import React from 'react'
+import { Col, Container, Row } from "react-bootstrap";
+import { IoLogoTwitter } from "react-icons/io5";
+import logo from "../assets/img/logo.png"
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <Container>
+        <Row>
+          <Col xs={12}>
+          <div className="footer-content">
+            <Row className="justify-content-between">
+              <Col xs={12} md={3}>
+              <div className="footer-logo"><img src={logo} alt="" /></div>
+              </Col>
+              <Col xs={12} md={6} >
+              <Row>
+                <Col xs={12} md={4} >
+                <p>Menu</p>
+                <ul>
+                  <li>Institutional</li>
+                  <li>Fee Discount</li>
+                  <li>API Documentation</li>
+                </ul>
+                </Col>
+                <Col xs={12} md={4} >
+                <p>Menu</p>
+                <ul>
+                  <li>FAQ</li>
+                  <li>Privacy Policy</li>
+                  <li>Terms & Conditions</li>
+                </ul>
+                </Col>
+                <Col xs={12} md={4} >
+                <p>Menu</p>
+                <ul>
+                  <li>linSPIRIT</li>
+                  <li>liHND</li>
+                  <li>Bond</li>
+                </ul>
+                </Col>
+              </Row>
+              </Col>
+              <Col xs={12} md={3} >
+              <div className="footer-social">
+                <p>Follow us</p>
+                <div>
+                  <a href="#"><IoLogoTwitter/></a>
+                </div>
+              </div>
+              </Col>
+            </Row>
+          </div>
+          </Col>
+          <Col xs={12} md={6}>
+            <div className="copy-right d-flex justify-content-between ">
+              <p>Copyright © 2024 Meme club . All Rights Reserved</p>
+              <p>Terms of Use ｜ Privacy Policy</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Col, Container, Row } from "react-bootstrap";
+import logo from "../assets/img/logo.png"
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <Container>
+        <Row>
+          <Col xs={12}>
+          <div className="header-nav d-flex justify-content-between align-content-center">
+            <div className="logo"><img src={logo} alt="" /></div>
+            <button className="btn">Connect Wallet</button>
+          </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
