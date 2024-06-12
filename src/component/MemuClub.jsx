c4b5ea42418ea4aaadb809d7e0cc86452027c625
Hassan Ali <hassan4np@gmail.com>
2024-06-12 13:12:56 +0600

    complate cube sectin

--- src/component/MemuClub.jsx
+++ src/component/MemuClub.jsx
@@ -1,32 +1,55 @@
 import { Col, Container, Row } from "react-bootstrap";
-
+import icon from "../assets/img/cardicon.png"
+import icon2 from "../assets/img/cardicon2.png"
+import icon3 from "../assets/img/cardcion3.png"
+import shap from "../assets/img/clubshap.png"
 export default function MemuClub() {
+  const cards=[
+    {
+      icon:`${icon}`,
+      title:"Incentivizing Longer Holding",
+      des:"By allowing memecoin holders to stake their tokens, Meme Club enables users to accrue yield and earn rewards. "
+    },
+    {
+      icon:`${icon2}`,
+      title:"Increasing Retention",
+      des:"Staking rewards encourage users to retain their memecoins for extended periods, stabilizing their value and promoting a more resilient market."
+    },
+    {
+      icon:`${icon3}`,
+      title:"Supporting the Meme Economy",
+      des:"Extending the life span of memecoins beyond the typical 72 hours contributes to the overall health and growth of the meme economy."
+    },
+  ]
   return (
-    <div className="menuclub">
+    <div className="menuclub position-relative z-0">
+      <div className="menuclub-shap position-absolute z-n1"><img src={shap} alt="" /></div>
        <Container>
        <Row className="justify-content-center">
-          <Col xs={12} md={7}>
-          <div className="menuclub-heading text-center">
+          <Col xs={12} md={8}>
+          <div className="menuclub-heading text-center w-100  d-flex flex-column">
             <h1>Why Meme Club?</h1>
             <p>The memecoin market is known for its rapid fluctuations and short life spans, often lasting only about 72 hours. This volatility and the holders  short attention spans can undermine the stability and growth of the meme economy. Meme Club addresses these issues head-on with a staking protocol that incentivizes longer holding and increases retention.</p>
           </div>
           </Col>
         </Row>
-        <Row>
-          <Col xs={12} md={4}>
+        <Row className="g-0">
+        {
+          cards.map((item,idx)=><Col xs={12} md={4} key={idx} className="d-flex" >
           <div className="menuclub-cards">
-           <div><img src="" alt="" /></div>
-           <h1>Incentivizing Longer Holding</h1>
-           <p>By allowing memecoin holders to stake their tokens, Meme Club enables users to accrue yield and earn rewards. </p>
-           <div>
-            <button>Read more</button>
-            <a href="#"><svg width="22" height="21" viewBox="0 0 22 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+           <div className="image"><img src={item.icon} alt="" /></div>
+           <h3 className="mb-2 md-mb-4">{item.title}</h3>
+           <p>{item.des}</p>
+           <div className="d-flex align-items-center mt-auto">
+              <button>Read more</button>
+              <a href="#" className="d-flex align-items-center "><svg width="27" height="20" viewBox="0 0 22 21" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M18.3486 6.07811L18.3431 14.8718L21.8017 14.8788L21.8315 0.161358L7.11404 0.131604L7.10704 3.5902L15.9007 3.62024L0.944573 18.516L3.39252 20.9739L18.3486 6.07811Z" fill="white"/>
               </svg>
             </a>
            </div>
           </div>
-          </Col>
+          </Col>)
+        }
         </Row>
        </Container>
     </div>
--- src/component/MemuWorks.jsx
+++ src/component/MemuWorks.jsx
@@ -1,7 +1,36 @@
+import { Col, Container, Row } from "react-bootstrap";
 
 
 export default function MemuWorks() {
   return (
-    <div>MemuWorks</div>
+    <div className="work">
+      <Container>
+      <div className="work-wrapper">
+      <Row>
+          <Col xs={12} md={6} >
+           <div className="work-title">     <h2> How Does Meme Club Work?</h2></div>
+          </Col>
+          <Col xs={12} md={6} >
+       <div className="work-des">   <p> Meme Club operates through a streamlined and user-friendly 
+       process designed to make staking easy and rewarding for both project and user</p></div>
+          </Col>
+      </Row>
+      <div className="work-wrapper-cards">
+       <Row>
+        <Col xs={12} md={4} ></Col>
+        <Col xs={12} md={8} >
+        <div className="work-item">
+          <div className="work-card">
+            <h4>Partnerships with Top Trading Memecoins</h4>
+            <p>We collaborate with leading memecoin projects, seamlessly integrating them onto our platform. This ensures a diverse range of staking options for users and enhances the ecosystem’s overall liquidity and stability.</p>
+            <div ><img src="" alt="" /></div>
+          </div>
+        </div>
+        </Col>
+       </Row>
+      </div>
+      </div>
+      </Container>
+    </div>
   )
 }
